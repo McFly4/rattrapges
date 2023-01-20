5d46c73e1e529f9a4d30509d183c8231addf8704
McFly4 <matthieu.roman2002@gmail.com>
2023-01-20 15:45:33 +0100

    done

--- src/Tableau.js
+++ src/Tableau.js
@@ -1,7 +1,33 @@
+import data from "./data/users.json";
+
 function Tableau() {
     return (
         <div className="tableau">
             <h1>Tableau</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((user) => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.phone}</td>
+                            <td>
+                                <a href={`mailto:${user.email}`}>
+                                    {user.email}
+                                </a>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
